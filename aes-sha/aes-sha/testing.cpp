a1732ef44a4601e60c16929dc3c119e51fd7da0c
NeminiCZ <m.minatova@gmail.com>
2016-03-07 11:28:57 +0100

    tests

--- aes-sha/aes-sha/testing.cpp
+++ aes-sha/aes-sha/testing.cpp
@@ -23,6 +23,7 @@ TEST_CASE("Encryption -> Decryption test") {
 	unsigned char iv[16] = { 0x0F, 0x02, 0x08, 0x04, 0x03, 0x08, 0x06, 0x0F, 0x0E, 0x07, 0x05, 0x07, 0x0D, 0x05, 0x07, 0x0D };
 	cryptFun crypto(key, iv);
 	cryptFun crypto1(key, iv);
+	cryptFun crypto2(key, iv);
 	cryptFun cryptoDifKey(key2, iv);
 	unsigned char* vanillaFileContents = NULL;
 	unsigned char* encFileContents = NULL;
@@ -36,7 +37,44 @@ TEST_CASE("Encryption -> Decryption test") {
 		CHECK(crypto.encryptAndHash(vanillaFileContents, vanillaFileSize, encryptOutput, encryptOutputSize) == 0);
 		CHECK(crypto1.decryptAndVerify(encryptOutput, encryptOutputSize, decryptOutput, decryptOutputSize) == 0);
 	}
+	crypto.readFile("testRead.txt", vanillaFileContents, vanillaFileSize);
 	SECTION("dec with wrong key") {
+		CHECK(crypto2.encryptAndHash(vanillaFileContents, vanillaFileSize, encryptOutput, encryptOutputSize) == 0);
 		CHECK(cryptoDifKey.decryptAndVerify(encryptOutput, encryptOutputSize, decryptOutput, decryptOutputSize) == 1);
 	}
+	delete[] vanillaFileContents;
+	delete[] encFileContents;
+	delete[] encryptOutput;
+	delete[] decryptOutput;
+}
+
+TEST_CASE("Encryption vector") {
+	unsigned char key[16] = { 0x09, 0x0F, 0x05, 0x0C, 0x0A, 0x0B, 0x0B, 0x0C, 0x0D, 0x0D, 0x0E, 0x03, 0x04, 0x05, 0x07, 0x0D };
+	unsigned char key2[16] = { 0x09, 0x0F, 0x05, 0x0C, 0x0A, 0x0B, 0x0B, 0x0C, 0x0D, 0x0D, 0x0E, 0x03, 0x04, 0x05, 0x08, 0x0D };
+	unsigned char iv[16] = { 0x0F, 0x02, 0x08, 0x04, 0x03, 0x08, 0x06, 0x0F, 0x0E, 0x07, 0x05, 0x07, 0x0D, 0x05, 0x07, 0x0D };
+	cryptFun crypto(key, iv);
+	cryptFun crypto1(key, iv);
+	cryptFun crypto2(key, iv);
+	cryptFun cryptoDifKey(key2, iv);
+	unsigned char* vanillaFileContents = NULL;
+	unsigned char* encryptOutput = NULL;
+	unsigned char* encLoadedFileContents = NULL;
+	unsigned char* corruptedFileContents = NULL;
+	size_t vanillaFileSize;
+	size_t encryptOutputSize;
+	size_t corruptedFileSize;
+	size_t encLoadedFileSize;
+	crypto.readFile("test.txt", vanillaFileContents, vanillaFileSize);
+	crypto.readFile("test.enc.txt", encLoadedFileContents, encLoadedFileSize);
+	crypto.readFile("corrupted.enc.txt", corruptedFileContents, corruptedFileSize);
+	SECTION("readFile  -> enc") {
+		CHECK(crypto.encryptAndHash(vanillaFileContents, vanillaFileSize, encryptOutput, encryptOutputSize) == 0);
+		CHECK(encLoadedFileSize == encryptOutputSize);
+		CHECK(memcmp(encryptOutput, encLoadedFileContents, encryptOutputSize) == 0);
+		CHECK("corrupted file", memcmp(encryptOutput, corruptedFileContents, encryptOutputSize) != 0);
+	}
+	delete[] vanillaFileContents;
+	delete[] encryptOutput;
+	delete[] encLoadedFileContents;
+	delete[] corruptedFileContents;
 }
